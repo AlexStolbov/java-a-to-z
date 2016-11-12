226ce08b9b046af197e64fe2a4f3fd1f9ccfc04b
AlexStolbov <stolbov_alexey@mail.ru>
2016-11-12 09:41:37 +0300

    delete left tab in code

--- Chapter_001/Lesson4/src/main/java/ru/astolbov/Square.java
+++ Chapter_001/Lesson4/src/main/java/ru/astolbov/Square.java
@@ -1,56 +1,56 @@
 package ru.astolbov;
 
-   /**
-	* Class Square.
-	* Вычисление функции кв. уравнения.
-	* @author astolbov
-	* @since 10.11.2016
+/**
+* Class Square.
+* Вычисление функции кв. уравнения.
+* @author astolbov
+* @since 10.11.2016
+*/
+
+public class Square {
+	/**
+	* коэфф. при x^2.
 	*/
+	private float a;
+	/**
+	* коэфф. при x.
+	*/
+	private float b;
+	/**
+	* коэфф.
+	*/
+	private float c;
 
-	public class Square {
-		/**
-		* коэфф. при x^2.
-		*/
-		private float a;
-		/**
-		* коэфф. при x.
-		*/
-		private float b;
-		/**
-		* коэфф.
-		*/
-		private float c;
+   /**
+	* Конструктор.
+	* @param seta коэфф. при x^2
+	* @param setb коэфф. при x
+	* @param setc коэфф
+	*/
+	public Square(float seta, float setb, float setc) {
+		this.a = seta;
+		this.b = setb;
+		this.c = setc;
+	}
 
-	   /**
-		* Конструктор.
-		* @param seta коэфф. при x^2
-		* @param setb коэфф. при x
-		* @param setc коэфф
-		*/
-		public Square(float seta, float setb, float setc) {
-			this.a = seta;
-			this.b = setb;
-			this.c = setc;
+   /**
+	* Вычисляет значение функции для заданного x.
+	* @param x переменная уравнения
+	* @return y - значение функции
+	*/
+	public double calculate(int x) {
+		return Math.pow(x, 2) * this.a + x * this.b + this.c;
+	}
+
+   /**
+	* Выводит значения функции в консоль.
+	* @param start - начальное значение
+	* @param finish - конечное значение
+	* @param step - шаг
+	*/
+	public void show(int start, int finish, int step) {
+		for (int i = start; i <= finish; i += step) {
+			System.out.printf("x = %1d  y = %2f %n m", i, calculate(i));
 		}
-
-	   /**
-		* Вычисляет значение функции для заданного x.
-		* @param x переменная уравнения
-		* @return y - значение функции
-		*/
-		public double calculate(int x) {
-			return Math.pow(x, 2) * this.a + x * this.b + this.c;
-		}
-
-	   /**
-		* Выводит значения функции в консоль.
-		* @param start - начальное значение
-		* @param finish - конечное значение
-		* @param step - шаг
-		*/
-		public void show(int start, int finish, int step) {
-			for (int i = start; i <= finish; i += step) {
-				System.out.printf("x = %1d  y = %2f %n m", i, calculate(i));
-			}
-		}
+	}
 }
--- Chapter_001/Lesson4/src/main/java/ru/astolbov/SquareCalculate.java
+++ Chapter_001/Lesson4/src/main/java/ru/astolbov/SquareCalculate.java
@@ -1,24 +1,24 @@
 package ru.astolbov;
 
-   /**
-	* Class SquareCalculate.
-	* Создание и вызов вычисления квадратного уровнения.
-	* @author astolbov
-	* @since 11.11.2016
-	*/
+/**
+* Class SquareCalculate.
+* Создание и вызов вычисления квадратного уровнения.
+* @author astolbov
+* @since 11.11.2016
+*/
 
-	public class SquareCalculate {
+public class SquareCalculate {
 
-		public static void main(String[] args) {
-			final int COEFF_A = 10;
-			final int COEFF_B = 15;
-			final int COEFF_C = 20;
-			final int START_X = 20;
-			final int FINISH_X = 20;
-			final int STEP_X = 20;
+	public static void main(String[] args) {
+		final int COEFF_A = 10;
+		final int COEFF_B = 15;
+		final int COEFF_C = 20;
+		final int START_X = 20;
+		final int FINISH_X = 20;
+		final int STEP_X = 20;
 
-			System.out.printf("a = %1d; b = %2d; c = %3d %n", COEFF_A, COEFF_B, COEFF_C);
-			Square square = new Square(COEFF_A, COEFF_B, COEFF_C);
-			square.show(START_X, FINISH_X, STEP_X);
-		}
+		System.out.printf("a = %1d; b = %2d; c = %3d %n", COEFF_A, COEFF_B, COEFF_C);
+		Square square = new Square(COEFF_A, COEFF_B, COEFF_C);
+		square.show(START_X, FINISH_X, STEP_X);
+	}
 }
