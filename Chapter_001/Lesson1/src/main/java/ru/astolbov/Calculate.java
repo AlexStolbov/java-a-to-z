8d336531139152bcb89288ce7e0919e84d343434
StolbovAlexey <stolbov_alexey@mail.ru>
2016-12-04 23:17:14 +0300

    change tab to space

--- Chapter_001/Lesson1/src/main/java/ru/astolbov/Calculate.java
+++ Chapter_001/Lesson1/src/main/java/ru/astolbov/Calculate.java
@@ -2,6 +2,7 @@ package ru.astolbov;
 
 /**
  * Class Calculate решение задачи части 001 урок1.
+ *
  * @author stolbov
  * @since 05.11.2016
  */
@@ -21,7 +22,7 @@ public class Calculate {
      *
      * @param value - строка для вывода в консоль
      * @return - echo for param
-     * */
+     */
 
     public String echo(String value) {
         return String.format("%s %s %s", value, value, value);
