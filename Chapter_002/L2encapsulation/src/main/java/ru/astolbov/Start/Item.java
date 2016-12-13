b6757977e215f86bec5981ba5c73a4a5117c7b1f
StolbovAlexey <stolbov_alexey@mail.ru>
2016-12-13 20:38:32 +0300

    add class Item

--- Chapter_002/L2encapsulation/src/main/java/ru/astolbov/Start/Item.java
+++ Chapter_002/L2encapsulation/src/main/java/ru/astolbov/Start/Item.java
@@ -1,7 +1,20 @@
 package ru.astolbov.Start;
 
+import ru.astolbov.Start.Comment;
+
 /**
  * Created by alex on 12/13/16.
  */
 public class Item {
+    private String id;
+    private String name;
+    private String description;
+    private Comment[] comments;
+    private int createDate;
+
+    public Item(String setid, String setname){
+        this.id = setid;
+        this.name = setname;
+    }
+
 }
