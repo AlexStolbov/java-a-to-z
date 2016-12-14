9b79f0c753bee86f43d222d1209a88089d8abe4e
StolbovAlexey <stolbov_alexey@mail.ru>
2016-12-14 21:30:05 +0300

    add Comment and Item

--- Chapter_002/L2encapsulation/src/main/java/ru/astolbov/Start/Comment.java
+++ Chapter_002/L2encapsulation/src/main/java/ru/astolbov/Start/Comment.java
@@ -1,25 +1,25 @@
 package ru.astolbov.Start;
 
 /**
+ * Комментарии к задачам
  * Created by alex on 12/13/16.
  */
 public class Comment {
-    private String id;
     private String commentText;
 
-    public void setId(String setId){
-        this.id = setId;
-    }
-
-    public String getId(){
-        return this.id;
-    }
-
-    public void setComment(String setCommentText){
+    /**
+     * Setter commentTest.
+     * @param setCommentText
+     */
+    public void setComment(String setCommentText) {
         this.commentText = setCommentText;
     }
 
-    public String getComment(){
+    /**
+     * Getter commentText
+     * @return
+     */
+    public String getComment() {
         return this.commentText;
     }
 }
--- Chapter_002/L2encapsulation/src/main/java/ru/astolbov/Start/Item.java
+++ Chapter_002/L2encapsulation/src/main/java/ru/astolbov/Start/Item.java
@@ -3,18 +3,81 @@ package ru.astolbov.Start;
 import ru.astolbov.Start.Comment;
 
 /**
+ * Item
  * Created by alex on 12/13/16.
  */
 public class Item {
     private String id;
     private String name;
     private String description;
-    private Comment[] comments;
+    private Comment[] comments = new Comment[5];
     private int createDate;
+    private int lastPositionComment = 0;
 
-    public Item(String setid, String setname){
+    /**
+     * Setter id
+     * @param setid
+     */
+    public void setId(String setid){
         this.id = setid;
-        this.name = setname;
     }
 
+    /**
+     * Getter id
+     * @return id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Setter name
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Getter name
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter description
+     * @param description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Getter description
+     * @return
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Add new comment into comments
+     * @param commentText
+     */
+    public void addComment(String commentText) {
+        if (lastPositionComment < comments.length) {
+            comments[lastPositionComment] = new Comment();
+            comments[lastPositionComment].setComment(commentText);
+            lastPositionComment++;
+        }
+    }
+
+    /**
+     * Getter comments
+     */
+    public Comment[] getComments() {
+        return comments;
+    }
 }
