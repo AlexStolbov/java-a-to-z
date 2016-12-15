a6066e770d89daa330d6eae4a9d878dd7f8c2d75
StolbovAlexey <stolbov_alexey@mail.ru>
2016-12-15 21:39:36 +0300

    edit calss Tracker add class TrackerTest

---
+++ Chapter_002/L2encapsulation/src/test/java/ru/astolbov/Start/TrackerTest.java
@@ -0,0 +1,24 @@
+package ru.astolbov.Start;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class TrackerTest.
+ * Тест класса Tracker
+ * @author stolbov
+ * @since 15.12.2016
+ */
+
+public class TrackerTest {
+
+	/**
+	* Tracker.
+	*/
+	@Test
+	public void whenGenerateIdThenIdNotUse() {
+
+		assertThat(true, is(false));
+	}
+}
---
+++ Chapter_002/L2encapsulation/src/test/java/ru/astolbov/Start/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by alex on 12/15/16.
+ */
+package ru.astolbov.Start;
