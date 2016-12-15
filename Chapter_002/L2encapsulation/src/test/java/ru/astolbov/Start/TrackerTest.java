cae342ee76c3830d1e1bbf4aa94f6484c13ee342
StolbovAlexey <stolbov_alexey@mail.ru>
2016-12-15 22:56:53 +0300

    edit class TrackerTest

--- Chapter_002/L2encapsulation/src/test/java/ru/astolbov/Start/TrackerTest.java
+++ Chapter_002/L2encapsulation/src/test/java/ru/astolbov/Start/TrackerTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import ru.astolbov.Models.Item;
+
 /**
  * Class TrackerTest.
  * Тест класса Tracker
@@ -12,13 +14,37 @@ import static org.junit.Assert.assertThat;
  */
 
 public class TrackerTest {
+    /**
+     * instance of class Tracker for test.
+     */
+    private Tracker tracker = null;
 
-	/**
-	* Tracker.
-	*/
+    /**
+     * Creates test instance.
+     */
+    private void createTracker() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+    }
+
+    /**
+     * Test add item in tracker.
+     * First add an item to the tracker, then finding it.
+     */
 	@Test
-	public void whenGenerateIdThenIdNotUse() {
-
-		assertThat(true, is(false));
+	public void whenAddItemThenTrackerGetIt() {
+	    createTracker();
+        Item item = new Item();
+        tracker.addItem(item);
+        Item[] items = tracker.getItems();
+        boolean findItem = false;
+        for (Item currentItem : items) {
+            if (currentItem.equals(item)) {
+                findItem = true;
+                break;
+            }
+        }
+		assertThat(findItem, is(true));
 	}
 }
