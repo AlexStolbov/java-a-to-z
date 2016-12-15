707f045a666a7e3343f0a80910994ad98e55e900
StolbovAlexey <stolbov_alexey@mail.ru>
2016-12-15 19:43:48 +0300

    edit class Tracker

--- Chapter_002/L2encapsulation/src/main/java/ru/astolbov/Start/Tracker.java
+++ Chapter_002/L2encapsulation/src/main/java/ru/astolbov/Start/Tracker.java
@@ -1,6 +1,7 @@
 package ru.astolbov.Start;
 
 import ru.astolbov.Models.Item;
+import java.util.Random;
 
 /**
  * Tracker
@@ -8,8 +9,35 @@ import ru.astolbov.Models.Item;
  */
 public class Tracker {
     /**
-     * Set of items.
+     * Array of items.
      */
     private Item[] items = new Item[10];
 
+    /**
+     * Last added position in this.items.
+     */
+    private int lastItemsPos = 0;
+
+    /**
+     * Create random items id.
+     */
+    private final Random rN = new Random();
+
+    /**
+     * Add new item.
+     * @param item - new item.
+     */
+    public void addItem(Item item) {
+        item.setId(generateId());
+        items[lastItemsPos] = item;
+        lastItemsPos++;
+    }
+
+    /**
+     * Generate new items id.
+     * @return new id
+     */
+    private String generateId() {
+        return Integer.toString(rN.nextInt(100000));
+    }
 }
