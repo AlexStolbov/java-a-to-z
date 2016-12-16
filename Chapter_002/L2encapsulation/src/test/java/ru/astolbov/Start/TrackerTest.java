388d15fdd35986fc4f43d73f26550fb4588b3613
StolbovAlexey <stolbov_alexey@mail.ru>
2016-12-16 19:44:53 +0300

    edit class TrackerTest

--- Chapter_002/L2encapsulation/src/test/java/ru/astolbov/Start/TrackerTest.java
+++ Chapter_002/L2encapsulation/src/test/java/ru/astolbov/Start/TrackerTest.java
@@ -47,4 +47,55 @@ public class TrackerTest {
         }
 		assertThat(findItem, is(true));
 	}
+
+    /**
+     * Test edit item in tracker.
+     */
+    @Test
+    public void whenEditItemThenItemHaveNewState() {
+        createTracker();
+        Item item = new Item();
+        tracker.addItem(item);
+        String newName = "Get work";
+        String newDescription = "time is money";
+        tracker.editItem(item, newName, newDescription);
+        boolean itemIsEdit;
+        itemIsEdit = (item.getName().equals(newName) & item.getDescription().equals(newDescription));
+
+        assertThat(itemIsEdit, is(true));
+    }
+
+    /**
+     * Test deleting item.
+     */
+    @Test
+    public void whenDeleteItemThenTrackerHasNoIt() {
+        createTracker();
+        Item item = new Item();
+        tracker.addItem(item);
+        Item[] items = tracker.getItems();
+        tracker.deleteItem(item);
+
+        boolean findItem = false;
+        for (Item currentItem : items) {
+            if (currentItem != null) {
+                if (currentItem.equals(item)) {
+                    findItem = true;
+                    break;
+                }
+            }
+        }
+        assertThat(findItem, is(false));
+    }
+
+    /**
+     * Test getter items.
+     */
+    public void whenGetItemsThenGetItems() {
+        createTracker();
+        Item item = new Item();
+        tracker.addItem(item);
+        Item[] items = tracker.getItems();
+        assertThat(items != null, is(true));
+    }
 }
